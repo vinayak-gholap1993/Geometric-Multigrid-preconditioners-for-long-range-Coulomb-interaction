b22b99f0c07b312d384cac0cb8af4a728747d865
vinayakgholap <vinayak.gholap@fau.de>
2017-10-27 18:43:11 +0200

    To be fixed for optimization (WIP)

---
+++ src/main.cc
@@ -0,0 +1,83 @@
+#include <step_50.h>
+using namespace dealii;
+
+
+
+int main (int argc, char *argv[])
+{
+    dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+    try
+    {        
+        using namespace Step50;
+
+        //deallog.depth_console(3);
+
+        AssertThrow(argc > 1, ExcMessage ("Invalid inputs"));
+
+        std::string parameter_name (argv[1]);
+
+        ParameterHandler prm;
+        ParameterReader param(prm);
+        param.declare_parameters();
+        param.read_parameters(parameter_name);
+
+        prm.enter_subsection("Problem Selection");
+        std::string Problemtype= (prm.get("Problem"));
+        const unsigned int d = prm.get_integer("Dimension");    // set default to two in parameter class
+        prm.leave_subsection();
+
+        prm.enter_subsection("Solver input data");
+        std::string PreconditionerType = (prm.get("Preconditioner"));
+        prm.leave_subsection();
+
+        prm.enter_subsection("Lammps data");
+        std::string LammpsInputFile = (prm.get("Lammps input file"));
+        prm.leave_subsection();
+
+        const unsigned int Degree = prm.get_integer("Polynomial degree");
+
+        if (d == 2)
+        {
+            LaplaceProblem<2> laplace_problem(Degree , prm ,Problemtype, PreconditionerType, LammpsInputFile);
+            laplace_problem.run ();
+        }
+        else if (d == 3)
+        {
+            LaplaceProblem<3> laplace_problem(Degree , prm ,Problemtype, PreconditionerType, LammpsInputFile);
+            laplace_problem.run ();
+        }
+        else if (d != 2 && d != 3)
+        {
+            AssertThrow(false, ExcMessage("Only 2d and 3d dimensions are supported."));
+        }
+
+
+    }
+    catch (std::exception &exc)
+    {
+        std::cerr << std::endl << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        std::cerr << "Exception on processing: " << std::endl
+                  << exc.what() << std::endl
+                  << "Aborting!" << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        throw;
+    }
+    catch (...)
+    {
+        std::cerr << std::endl << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        std::cerr << "Unknown exception!" << std::endl
+                  << "Aborting!" << std::endl
+                  << "----------------------------------------------------"
+                  << std::endl;
+        throw;
+    }
+
+    return 0;
+}
+
